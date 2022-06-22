257bedff3ba7f4f30efa36a6ff5c5fc162ecd244
AnushaPanuganti <107758299+AnushaPanuganti@users.noreply.github.com>
2022-06-22 22:36:17 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -19,22 +19,30 @@ def login():
         
         if db.users.find_one({"username": username, "password": password}):
             return "Login success"
-
         return redirect('/register')
 
 
-@app.route('/register')
+@app.route('/register',methods=['GET', 'POST'])
 def register():
     #TODO: 
     # If req is get -> return register.html
+    if request.method == "GET":
+        return render_template("register.html")
     # else -> parse request object, 
-    # if username is in db:
-    #     redirect to login
-    # else:  save user details into db
-    # show a success message
-    
-    
-    return "You are in register page"
+    else:
+        username = request.form['username']
+        password = request.form['set password']
+        # if username is in db:
+        if db.users.find_one({"username": username}):
+            print("user already exists")
+            return redirect('/login')
+
+        #     redirect to login
+        # else:  save user details into db
+        else:
+            db.users.insert_one({"username": username, "password": password})
+        # show a success message
+            return 'Registered successfully,<a href= "/login" >Click here to login</a>'
 
 @app.route('/homepage')
 def homepage():
--- config.py
+++ config.py
@@ -2,16 +2,16 @@
 import urllib.parse
 
 DB_NAME="weatherpapp"
-DB_USERNAME="anandkulkarni91"
-DB_PASSWORD="Anand@7097"
+DB_USERNAME="anushasweety17"
+DB_PASSWORD="Panuganti"
 
-DB_CONNECTION_STRING = f"mongodb+srv://{urllib.parse.quote(DB_USERNAME)}:{urllib.parse.quote(DB_PASSWORD)}@cluster0.pvcf8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DB_CONNECTION_STRING = f"mongodb+srv://{urllib.parse.quote(DB_USERNAME)}:{urllib.parse.quote(DB_PASSWORD)}@cluster0.fdnorpi.mongodb.net/?retryWrites=true&w=majority"
 NOTES_CHARACTER_LIMIT = 250
 
 from pymongo import MongoClient
 client = MongoClient(DB_CONNECTION_STRING)
 db = client[DB_NAME]
-
+print("connection established")
 user = db.users.find_one({"username": "root", "password":"root"})
 if not user:
         
